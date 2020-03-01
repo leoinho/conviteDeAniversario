a6e44a157bc39d9ccabcf3663b58299b13fa1a36
Leonardo Menezes da ConceiÇão <leonardo.conceicao@fullbar.com.br>
2020-03-01 18:32:06 -0300

    adicionando audio e fazendo ajustes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   window.setInterval(mudaNome, 1000);
+  var audio = new Audio('./audio/06-The-Nutcracker-Suite-Opera-71A-Russian-Dance-Pyotr-Ilyich-Tchaikovsky.mp3');
+  
 
+  $('body').click(function() {
+    if(audio.currentTime != 0){
+      audio.pause();
+      audio.currentTime = 0;
+    }else{
+      audio.play();
+    }
+  });
   function mudaNome(){
     var nome1 = $('.nome1').css('display');
     var nome2 = $('.nome2').css('display');
